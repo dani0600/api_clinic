4cad239013e25828c719d6c9a353be5aca702827
RogerHoms <rhoms@novatilu.com>
2022-05-08 14:10:08 +0200

    add query for ageRanges

--- models/person.js
+++ models/person.js
@@ -322,11 +322,66 @@ async function addDocument(info) {
 }
 
 
+async function getAgeRanges(){
+  const collection = db.getCollection(personsCollectionName);
+  const aggCursor = await collection.aggregate([
+    {
+      '$project': {
+        'age': {
+          '$subtract': [
+            '$age', {
+              '$mod': [
+                '$age', 1
+              ]
+            }
+          ]
+        }, 
+        'sex': 1
+      }
+    }, {
+      '$project': {    
+        "range": {
+           $concat: [
+              { $cond: [{$lte: ["$age",0]}, "Unknown", ""]}, 
+              { $cond: [{$and:[ {$gt:["$age", 0 ]}, {$lt: ["$age", 18]}, {$eq: ["$sex", "male"]}]}, "1b", ""] },
+              { $cond: [{$and:[ {$gt:["$age", 0 ]}, {$lt: ["$age", 18]}, {$eq: ["$sex", "female"]}]}, "1a", ""] },
+              { $cond: [{$and:[ {$gte:["$age",18]}, {$lt:["$age", 31]}, {$eq: ["$sex", "female"]}]}, "2a", ""]},
+              { $cond: [{$and:[ {$gte:["$age",18]}, {$lt:["$age", 31]}, {$eq: ["$sex", "male"]}]}, "2b", ""]},
+              { $cond: [{$and:[ {$gte:["$age",31]}, {$lt:["$age", 51]}, {$eq: ["$sex", "female"]}]}, "3a", ""]},
+              { $cond: [{$and:[ {$gte:["$age",31]}, {$lt:["$age", 51]}, {$eq: ["$sex", "male"]}]}, "3b", ""]},
+              { $cond: [{$and:[ {$gte:["$age",51]}, {$lt:["$age", 71]}, {$eq: ["$sex", "female"]}]}, "4a", ""]},
+              { $cond: [{$and:[ {$gte:["$age",51]}, {$lt:["$age", 71]}, {$eq: ["$sex", "male"]}]}, "4b", ""]},
+              { $cond: [{$and:[ {$gte:["$age",71]}, {$eq: ["$sex", "male"]}]}, "5a", ""]},
+              { $cond: [{$and:[ {$gte:["$age",71]}, {$eq: ["$sex", "female"]}]}, "5b", ""]}
+           ]
+        }  
+      }
+    }, {
+      '$group': {
+        '_id': {
+          'grupo': '$range'
+        }, 
+        'Total': {
+          '$sum': 1
+        }
+      }
+    }, {
+      '$sort': { 
+        "_id.grupo" : 1 
+      }
+    }
+  ]);
+  return await aggCursor.toArray();
+}
+
+
+
 module.exports = {
   checkProperties,
   getAll,
   add,
-  addDocument
+  addDocument,
+  getAgeRanges
 }
 
 
