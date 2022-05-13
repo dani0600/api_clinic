5e3afd960f8c184401dc6cb4caf5b0f956b6d6a2
unknown <danielvequi@gmail.com>
2022-05-13 19:53:27 +0200

    Add /suggestions endpoint

---
+++ routes/suggestions.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const suggestionModel = require('./../models/suggestion');
+const {endpointProtection} = require('../middlewares/endpoint.protection.middleware');
+const { checkProperties } = require("../models/relative");
+
+const router = express.Router()
+
+router.get('/', endpointProtection, async function(req, res, next) {
+    
+    let option = req.query.option;
+    if(option === 'mark'){
+        try {
+            const marks = await suggestionModel.getAvgMark();
+            res.status(200).send(marks);
+        }
+        catch(error){
+            next(error);
+        }
+    }
+    else if(option === 'count'){
+        try{
+            const sum = await suggestionModel.countTotalSuggestions();
+            res.json(sum);
+        }
+        catch(error){
+            next(error);
+        }
+    }
+    else{
+        try {
+            const suggestions = await suggestionModel.getAll(req.query);
+            res.status(200).send(suggestions);
+        }
+        catch(error){
+            next(error);
+        }
+    }
+})
+
+router.post('/', endpointProtection, async function(req, res, next) {
+    try {
+        const suggestions = await suggestionModel.add(req.body);
+        res.status(200).send(suggestions);
+    }
+    catch(error){
+        next(error);
+    }
+})
+
+module.exports = router;
